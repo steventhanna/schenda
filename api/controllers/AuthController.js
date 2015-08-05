ecb339bc0b1f1b162b7d55e61eccc47cdc6ff680
Preston Stosur-Bassett <preston@stosur.info>
2015-08-05 16:27:55 -0400

    removed the dumb fucking file with 'Obejct.keys' in it. and then also added support for auth

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -17,57 +17,76 @@ module.exports = {
 
   signup: function(req, res) {
     var post = req.body;
-    var uid = Math.floor(Math.random() * 1000000000000000000000);
 
-    var accountData = {
+    var accountDetails = {
       email: post.email,
-      firstName: post.firstName,
       password: post.password,
-      lastName: post.lastName,
-      uid: uid,
-      displayName: post.firstName + " " + post.lastName
-    };
+      displayName: post.firstName+" "+post.lastName,
+      firstName: post.firstName,
+      lastName: post.lastName
+   };
 
-    User.create(accountData).exec(function(err, user) {
-      if (err || user == undefined) {
-        console.log("There was an error when creating hte user account on the database.");
-        console.log("Error = " + err);
-        console.log(user);
-      } else {
-        req.logIn(user, function(err) {
-          if (err) {
-            res.serverError();
-            console.log("Could not log user in.");
-            return;
-          } else {
-            console.log("User logged in");
-            res.send({
-              success: true
-            });
-          }
-        });
+   User.create(accountDetails).exec(function(err, user) {
+      if(err || user == undefined) {
+         console.log("There was an error creating the user account on the database.");
+         console.log("Error = "+err);
+         console.log("Error Code 0001.0");
+         console.log("Account Details: ");
+         console.log(accountDetails);
+
+         res.serverError();
       }
-    });
+      else {
+         req.logIn(user, function(err) {
+            if(err) {
+               console.log("There was an error when trying to login the user after the account was just created.");
+               console.log("Error = "+err);
+               console.log("Error Code 0001.1");
+               console.log("User Account: ");
+               console.log(user);
+
+               res.serverError();
+               return;
+            }
+            else {
+               res.send({ success: true });
+            }
+         });
+      }
+   });
   },
 
   login: function(req, res) {
     passport.authenticate('local', function(err, user, info) {
-      if ((err) || (!user)) {
-        return res.send({
-          message: info.message,
-          user: user
-        });
+      if(err || (!user)) {
+         console.log("user = "+user);
+         console.log("err = "+err);
+         console.log("info = "+info);
+         res.view('auth/loginError');
       }
-      req.logIn(user, function(err) {
-        if (err) {
-          res.send(err);
-        }
-        return res.send({
-          message: info.message,
-          user: user
-        });
-      });
-    })(req, res);
+      else if((!err) && user) {
+         req.logIn(user, function(err) {
+            if(err) {
+               console.log("There was an error logging the user.");
+               console.log("Error = "+err);
+               console.log("Error Code 0002.0");
+               console.log("Users Account: ");
+               console.log(user);
+
+               res.serverError();
+               return;
+            }
+            else {
+               res.send({
+                  success: true
+               });
+            }
+         });
+      }
+      else {
+         res.send({ success: false });
+      }
+   })(req, res);
   },
 
   renderSignUp: function(req, res) {
--- config/application.js
+++ config/application.js
@@ -1,114 +1,92 @@
-/*
- * @File: 		config/application.js
- * @Created: 	July 2015
- * @Updated: 	July 27, 2015
- * @Description: This file handles logins and serial-desserializations
- * @Author: Steven T Hanna
- */
+/**
+* @File: config/application.js
+* @Created: August 5, 2015
+* @Description: This file handles logins and serial-deserializations
+* @Author: Preston Stosur-Bassett (www.stosur.info)
+*/
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt-nodejs');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 function findByUsername(username, fn) {
-  User.findOne({
-    email: username
-  }).exec(function(err, user) {
-    if (err || user == undefined) {
-      console.log("There was an error looking up the user with the meail " + username + " in the User Database.");
-      console.log("Error = " + err);
-      return fn("USER NOT FOUND ON DATABASE!", null);
-    } else {
-      return fn(null, user);
-    }
-  });
+   User.findOne({ email: username }).exec(function(err, user) {
+      if(err || user == undefined) {
+         console.log("There was an error looking up the user with email "+username+". ");
+         console.log("Error = "+err);
+         console.log("Error Code 2256");
+         return fn("USER NOT FOUND ON DATABASE! ERROR 2256.", null);
+      }
+      else {
+         return fn(null, user);
+      }
+   });
 }
 
-// Passport seession setup
-// To support persistent login session, Passport needs to be able to
-// serialize users into and deserialize users out of the session. Typically
-// this will be as simle as sotring the user ID when serializing, and finding
-// the user by ID when deserializing.
+//Passport session setup.
+//To support persistent login session, Passport needs to be able to
+//serialize users into and deserialize users out of the session. Typeically,
+//this will be as simple as storing the user ID when serializing, and finding
+//the user by ID when deserializing
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+   done(null, user.id);
 });
-
-// passport.deserializeUser(function(obj, done) {
-//   done(null, obj);
-// });
 
 passport.deserializeUser(function(id, done) {
-  User.findOne({
-    id: id
-  }).exec(function(err, user) {
-    if (err || user == undefined) {
-      console.log("There was an error looking up the user with the id " + id + " in the User Database");
-      console.log("Error = " + err);
-      return done("NOT IN THE USER DATABASE", null);
-    } else {
-      return done(null, user);
-    }
-  });
+   User.findOne({ id: id }).exec(function(err, user) {
+      if(err || user == undefined) {
+         console.log("There was an error looking up the user with the id "+id+". ");
+         console.log("Error = "+err);
+         console.log("Error code 1256");
+         return done("NOT IN THE USER DATABASE, ERROR 1256", null);
+      }
+      else {
+         return done(null, user);
+      }
+   });
 });
 
-// Use the LocalStrategy within Passport.
-// Strategies in passport require a 'verify' function, which accept
-// credentials (in this case, a username and password), and invoke a callback
-// with a user object.
-
-// passport.use(new GoogleStrategy({
-//     clientID: "139008887262-3uv5hn6m6mppsj0osaoeecv64nl8fpgs.apps.googleusercontent.com",
-//     clientSecret: "CQw1SSqI-3yCyvaJa0KBg7o-",
-//     callbackURL: "http://127.0.0.1:1337/auth/google/callback/"
-//   },
-//   function(token, tokenSecret, profile, done) {
-//     User.findOrCreate({
-//       googleId: profile.id
-//     }, function(err, user) {
-//       return done(err, user);
-//     });
-//   }
-// ));
-
+//User the LocalStrategy within Passport.
+//Strategies in passport requrie a verify function, which accept
+//credentials (in this case, a username and password), and invoke a callback,
+//with a user object
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    //asynchronous verification, for effect...
-    process.nextTick(function() {
-      //Find the user by username. If there is no user with the given username, or the password is not correct,s et the user to false to indicate failure and set a flash message.
-      //Otherwise, return the authenticated user
-      findByUsername(username, function(err, user) {
-        if (err) {
-          return done(err);
-        } else if (!user) {
-          return done("User Not Found", null, {
-            message: "Unknown user " + username
-          });
-        } else {
-          bcrypt.compare(password, user.password, function(err, res) {
-            if (res != true) {
-              return done("Invalid Password", null, {
-                message: "Invalid Password"
-              });
-            } else {
-              return done(null, user);
+   function(username, password, done) {
+      //asynchronous verification, for effect...
+      process.nextTick(function() {
+         //Find the user by username. If there is no user with the given username, or the password is not correct, set the user to false to indicate failure and send some sort of flash message or newer equaivalent
+         //Otherwise, return an authenticated user.
+         findByUsername(username, function(err, user) {
+            if(err) {
+               return done(err);
             }
-          });
-        }
+            else if(!user) {
+               return done("USER NOT FOUND", null, { message: "Unkown user"+username});
+            }
+            else {
+               bcrypt.compare(password, user.password, function(err, res) {
+                  if(res != true) {
+                     return done("INVALID PASSWORD", null, { message: "Invalid Password"});
+                  }
+                  else {
+                     return done(null, user);
+                  }
+               });
+            }
+         });
       });
-    });
-  }
+   }
 ));
 
 module.exports = {
-  appName: 'schenda',
+   appName: 'Schenda',
 
-  // Custom epresss middleware - we use this to register the passport middleware
-  http: {
-    customMiddleware: function(app) {
-      app.use(passport.initialize());
-      app.use(passport.session());
-      app.use(app.router);
-    }
-  }
+   //Custom express middleware - we use this to register the passwport middlware
+   http: {
+      customerMiddleWare: function(app) {
+         app.use(passport.initialize());
+         app.use(passport.session());
+         app.use(app.router);
+      }
+   }
 };
