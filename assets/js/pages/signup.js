067e0dac2b9f2e96a893dd7468dccb54f0de3c9f
Preston Stosur-Bassett <preston@stosur.info>
2015-08-05 16:51:36 -0400

    signup tells the user about how insecure they are

--- assets/js/pages/signup.js
+++ assets/js/pages/signup.js
@@ -1,30 +1,70 @@
 $(document).ready(function() {
-  $("submit").click(function() {
-    var firstName = $("#firstName").val();
-    var lastName = $("#lastName").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var passwordConfirmation = ("#passwordConfirmation").val();
+   $("#submit").click(function() {
+      var firstName = $("#firstName").val();
+      var lastName = $("#lastName").val();
+      var email = $("#email").val();
+      var password = $("#password").val();
+      var passwordConf = $("#passwordConfirmation").val();
 
-    // I'll handle error handling later.
+      function submitRequestForAccountCreation(profile) {
+         $.ajax({
+            type: 'POST',
+            url: '/signup',
+            data: profile,
+            success: function(data) {
+               console.log(data);
+               if(data.success == true) {
+                  window.location.href = "/dashboard";
+               }
+               else {
+                  swal("Uh-oh!", "Looks like there was some sort of error creating your account.", "error");
+               }
+            },
+            error: function(data) {
+               console.log(data);
+               swal("Uh-oh!", "Looks like there was some sort of error creating your account.", "error");
+            }
+         });
+      }
 
-    var postObj = {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: password,
-    };
+      if(password != passwordConf) {
+         swal("Password Mismatch", "Looks like the two passwords you typed don't match.", "error");
+      }
+      else {
+         if(password == "password") {
+            swal("Seriously?", "Having your password as 'password' is not security... You can do better than that...", "warning");
+         }
+         else if(password.length <= 7) {
+            swal({
+               title: "You're password is ridiculously insecure.",
+               text: "Are you sure you want to proceed using that short of a password?",
+               type: 'warning',
+               showCancelButton: true,
+               confirmButtonText: "Yes, I don't care about security.",
+               closeOnConfirm: true
+            }, function() {
+               swal("To hell with security!", "Creating your easily hackable account now.", "success");
 
-    $.ajax({
-        type: 'POST',
-        url: '/signup',
-        data: 'postObj'
-      }),
-      .success(function(data) {
-        alert("Success");
-      }),
-      .error(function(data) {
-        alert("Failure");
-      });
-  });
+               var accountDetails = {
+                  firstName: firstName,
+                  lastName: lastName,
+                  email: email,
+                  password: password
+               };
+
+               submitRequestForAccountCreation(accountDetails);
+            });
+         }
+         else {
+            var accountData = {
+               firstName: firstName,
+               lastName: lastName,
+               email: email,
+               password: password
+            };
+
+            submitRequestForAccountCreation(accountData);
+         }
+      }
+   });
 });
