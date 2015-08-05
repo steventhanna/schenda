6faad1e6c3e2b6576bdc7c6b86cf8caa0d28a95f
Preston Stosur-Bassett <preston@stosur.info>
2015-08-05 16:33:56 -0400

    fixed that dumb fucking but, Object keys and shit

--- api/models/User.js
+++ api/models/User.js
@@ -1,60 +1,68 @@
 /**
- * User.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/#!documentation/models
- */
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
 
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
 
   attributes: {
 
-    email: {
-      type: 'email',
-      required: true,
-      unique: true
-    },
+     //The User's email address, also used as the username on the site
+     email: {
+        required: true,
+        type: 'email',
+        unqiue: true
+     },
 
-    firstName: {
-      type: 'string',
-      required: true
-    },
+     //Stored a hashed version of the user's password
+     password: {
+        required: true,
+        type: 'string'
+     },
 
-    lastName: {
-      type: 'string',
-      required: true
-    },
+     //The users first name (given name)
+     firstName: {
+        required: true,
+        type: 'string'
+     },
 
-    displayName: {
-      type: 'string',
-      required: true
-    },
+     //The user's last name (Family name)
+     lastName: {
+        required: true,
+        type: 'string'
+     },
 
-    password: {
-      type: 'string',
-      required: true
-    },
+     //The user's display name (first name + last name)
+     //This only supports western formatted names, for example, a chinese name where the family names comes first is NOT supported in displayName
+     displayName: {
+        required: true,
+        type: 'string'
+     },
 
-    toJSON: function() {
-      var obj = this.toObject();
-      delete obj.password;
-      return obj;
-    }
+     //Remove the password before it is returned as a JSON object
+     toJSON: function() {
+        var obj = this.toObject();
+        delete obj.password;
+        return obj;
+     }
   },
 
+  //Hash the password before it is ever stored in the database
   beforeCreate: function(user, cb) {
-    bcrypt.getSalt(10, function(err, salt) {
-      bcrypt.hash(user.password, salt, function(err, hash) {
-        if (err) {
-          console.log(err);
-          cb(err);
-        } else {
-          user.password = hash;
-          cb();
-        }
-      });
-    });
-  }
+     bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(user.password, salt, function() {}, function(err, hash) {
+           if(err) {
+             console.log(err);
+          }
+          else {
+             user.password = hash;
+             cb(null, user);
+          }
+        });
+     });
+ }
 };
