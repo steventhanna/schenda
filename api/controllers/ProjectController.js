b6790cb2ade459688929b34e69699695add642fe
steventhanna <steventhanna@gmail.com>
2015-08-14 18:16:11 -0500

    Project page now operating correctly

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -76,7 +76,7 @@ module.exports = {
         console.log("Error Code 0003.0");
         res.serverError();
       } else {
-        // Get the cid
+        // Get the class id
         var url = req.url;
         var array = url.split("/");
         var index = user.classUrlNames.indexOf(array[2]);
@@ -89,31 +89,14 @@ module.exports = {
             console.log("Error = " + err);
             res.serverError();
           } else {
-            // Find all projects and send to page
-            if (className.projects.length == 0) {
-              console.log("NO PROJECTS");
-              res.view('dashboard/project', {
-                user: user,
-                currentPage: 'project',
-                classroom: className,
-                projects: undefined
-              });
-            } else {
+            // Gather all of the projects
+            if (className.projects.length > 0) {
               var projectIdList = className.projects;
               var fullProjectList = [];
-              if (fullProjectList.length == projectIdList.length) {
-                console.log("LISTS EQUAL");
-                res.view('dashboard/project', {
-                  user: user,
-                  currentPage: 'project',
-                  classroom: className,
-                  projects: fullProjectList
-                });
-              } else {
-                console.log("ENTER FOR LOOP");
+              var counter = 0;
+              if (fullProjectList.length < projectIdList.length) {
                 for (var i = 0; i < projectIdList.length; i++) {
                   if (fullProjectList.length == projectIdList.length) {
-                    console.log("LISTS EQUAL IN FOR LOOP");
                     res.view('dashboard/project', {
                       user: user,
                       currentPage: 'project',
@@ -121,19 +104,16 @@ module.exports = {
                       projects: fullProjectList
                     });
                   } else {
-                    console.log("LOOK UP PROJECT IN FOR LOOP");
                     Project.findOne({
                       pid: projectIdList[i]
                     }).exec(function(err, projectName) {
                       if (err || projectName == undefined) {
-                        console.log("The the project could not be located.");
+                        console.log("There was an error looking up the project.");
                         console.log("Error = " + err);
                         res.serverError();
                       } else {
-                        console.log("LOOPED UP PROJECT");
-                        fullProjectList[i] = projectName;
+                        fullProjectList[fullProjectList.length] = projectName;
                         if (fullProjectList.length == projectIdList.length) {
-                          console.log("LISTS EQUAL IN LOOK UP");
                           res.view('dashboard/project', {
                             user: user,
                             currentPage: 'project',
@@ -143,13 +123,124 @@ module.exports = {
                         }
                       }
                     });
+                    if (fullProjectList.length == projectIdList.length) {
+                      res.view('dashboard/project', {
+                        user: user,
+                        currentPage: 'project',
+                        classroom: className,
+                        projects: fullProjectList
+                      });
+                    }
                   }
                 }
+              } else {
+                res.view('dashboard/project', {
+                  user: user,
+                  currentPage: 'project',
+                  classroom: className,
+                  projects: fullProjectList
+                });
               }
+            } else {
+              console.log("NO PROJECTS");
+              res.view('dashboard/project', {
+                user: user,
+                currentPage: 'project',
+                classroom: className,
+                projects: undefined
+              });
             }
           }
         });
       }
     });
   },
+
+  // projects: function(req, res) {
+  //   var post = req.body;
+  //   User.findOne({
+  //     id: req.user.id
+  //   }).exec(function(err, user) {
+  //     if (err || user == undefined) {
+  //       console.log("There was an error looking up the logged in user.");
+  //       console.log("Error = " + err);
+  //       console.log("Error Code 0003.0");
+  //       res.serverError();
+  //     } else {
+  //       // Get the cid
+  //       var url = req.url;
+  //       var array = url.split("/");
+  //       var index = user.classUrlNames.indexOf(array[2]);
+  //       var cid = user.classes[index];
+  //       Classroom.findOne({
+  //         cid: cid
+  //       }).exec(function(err, className) {
+  //         if (err || className == undefined) {
+  //           console.log("There was an error looking up the class.");
+  //           console.log("Error = " + err);
+  //           res.serverError();
+  //         } else {
+  //           // Find all projects and send to page
+  //           if (className.projects.length == 0) {
+  //             console.log("NO PROJECTS");
+  //             res.view('dashboard/project', {
+  //               user: user,
+  //               currentPage: 'project',
+  //               classroom: className,
+  //               projects: undefined
+  //             });
+  //           } else {
+  //             var projectIdList = className.projects;
+  //             var fullProjectList = [];
+  //             if (fullProjectList.length == projectIdList.length) {
+  //               console.log("LISTS EQUAL");
+  //               res.view('dashboard/project', {
+  //                 user: user,
+  //                 currentPage: 'project',
+  //                 classroom: className,
+  //                 projects: fullProjectList
+  //               });
+  //             } else {
+  //               console.log("ENTER FOR LOOP");
+  //               for (var i = 0; i < projectIdList.length; i++) {
+  //                 if (fullProjectList.length == projectIdList.length) {
+  //                   console.log("LISTS EQUAL IN FOR LOOP");
+  //                   res.view('dashboard/project', {
+  //                     user: user,
+  //                     currentPage: 'project',
+  //                     classroom: className,
+  //                     projects: fullProjectList
+  //                   });
+  //                 } else {
+  //                   console.log("LOOK UP PROJECT IN FOR LOOP");
+  //                   Project.findOne({
+  //                     pid: projectIdList[i]
+  //                   }).exec(function(err, projectName) {
+  //                     if (err || projectName == undefined) {
+  //                       console.log("The the project could not be located.");
+  //                       console.log("Error = " + err);
+  //                       res.serverError();
+  //                     } else {
+  //                       console.log("LOOPED UP PROJECT");
+  //                       fullProjectList[i] = projectName;
+  //                       if (fullProjectList.length == projectIdList.length) {
+  //                         console.log("LISTS EQUAL IN LOOK UP");
+  //                         res.view('dashboard/project', {
+  //                           user: user,
+  //                           currentPage: 'project',
+  //                           classroom: className,
+  //                           projects: fullProjectList
+  //                         });
+  //                       }
+  //                     }
+  //                   });
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       });
+  //     }
+  //   });
+  // },
 };
