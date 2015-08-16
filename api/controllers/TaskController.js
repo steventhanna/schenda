6f05db3a11841c07be14a5cc706bee7fa5ebfcd4
steventhanna <steventhanna@gmail.com>
2015-08-16 10:37:59 -0500

    Controller cleanup

--- api/controllers/TaskController.js
+++ api/controllers/TaskController.js
@@ -131,65 +131,6 @@ module.exports = {
     });
   },
 
-  // status: function(req, res) {
-  //   var post = req.body;
-  //
-  //   User.findOne({
-  //     id: req.user.id
-  //   }).exec(function(err, user) {
-  //     if (err || user == undefined) {
-  //       console.log("There was an error looking up the logged in user.");
-  //       console.log("Error = " + err);
-  //       console.log("Error Code 0003.0");
-  //       res.serverError();
-  //     } else {
-  //       // Lookup classroom
-  //       Classroom.findOne({
-  //         cid: post.classId
-  //       }).exec(function(err, className) {
-  //         if (err || className == undefined) {
-  //           console.log("There was an error looking up the class.");
-  //           console.log("Error = " + err);
-  //           console.log("Error Code 00006.0");
-  //           res.serverError();
-  //         } else {
-  //           var tid = post.taskId;
-  //           var statusBool = post.status;
-  //           // If status == true, then complete.... else incomplete
-  //           if (statusBool == true) {
-  //             var status = 'complete';
-  //           } else {
-  //             var status = 'incomplete';
-  //           }
-  //           Task.findOne({
-  //             tid: tid
-  //           }).exec(function(err, taskName) {
-  //             if (err || taskName == undefined) {
-  //               console.log("There was an error looking up the task.");
-  //               console.log("Error = " + err);
-  //               console.log("Error Code 0012.0");
-  //               res.serverError();
-  //             }
-  //             taskName.status = status;
-  //             taskName.save(function(err) {
-  //               if (err) {
-  //                 console.log("The status on the task could not be updated");
-  //                 console.log("Error = " + err);
-  //                 console.log("Error Code 0011.0");
-  //                 res.serverError();
-  //               } else {
-  //                 res.send({
-  //                   success: true
-  //                 });
-  //               }
-  //             });
-  //           });
-  //         }
-  //       });
-  //     }
-  //   });
-  // },
-
   update: function(req, res) {
     var post = req.body;
     User.findOne({
